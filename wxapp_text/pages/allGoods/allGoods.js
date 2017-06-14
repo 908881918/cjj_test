9a3c0db7180a0efb1ced64907a3462e0db99cd87
908881918 <908881918@qq.com>
2017-06-15 00:47:04 +0800

    wxapp_text

---
+++ wxapp_text/pages/allGoods/allGoods.js
@@ -0,0 +1,65 @@
+// allGoods.js
+Page({
+  data: {
+    goods:[
+      {
+        img:"https://img.alicdn.com/bao/uploaded/i3/TB1ymMDIXXXXXatXXXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title:"乐魔 LOMO拍立得",
+        price:990,
+        sale:15
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i2/TB1rviBPpXXXXXoaXXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "【现货】LOMO`全自动拍立得AUTOMAT南滩特别版",
+        price: 1390,
+        sale: 9
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i1/TB1WUjDOVXXXXbhapXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "【现货】LOMO`全自动拍立得AUTOMAT",
+        price: 1290,
+        sale: 6
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i4/TB1ob4xKFXXXXcjXFXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "LOMOGRAPHY LOMO’宽画幅拍立得",
+        price: 1438,
+        sale: 3
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i3/TB1HHndPpXXXXXIXXXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "LOMO’拍立得相机–Murano【魔方】特别版",
+        price: 990,
+        sale: 9
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i4/TB1D625GFXXXXbDapXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "Diana F+ Instant Camera特别版",
+        price: 1580,
+        sale: 0
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i1/TB1ryxHHXXXXXcbXXXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "苏联经典复刻 LC-Wide",
+        price: 1980,
+        sale: 0
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i4/TB1D625GFXXXXbDapXXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "Lubitel 166+ 苏联经典复古",
+        price: 1680,
+        sale: 2
+      },
+      {
+        img: "https://img.alicdn.com/bao/uploaded/i4/T1eANiFnpXXXXXXXXX_!!0-item_pic.jpg_Q50s50.jpg_.webp",
+        title: "Belair X 6-12 City Slicker 黑色款",
+        price: 2180,
+        sale: 0
+      }
+    ],
+   goodsClass: [
+     "goods1",
+     "goods2"
+    ]
+  }
+})
